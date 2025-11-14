207f397a4bc10350cc058ea51e2ca5c628c89f3b
jeyley <abc160cba@gmail.com>
2025-11-15 00:30:59 +0700

    update/font-vs4

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import CartProvider from "./contexts/CartProvider";
 import SettingsProvider from "./contexts/SettingsProvider";
 import "./i18n/index.js";
 import './index.css';
+import './style/fonts.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
