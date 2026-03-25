19f7a61a731804337f61a5954cc8ab2f27095d86
jeyley <abc160cba@gmail.com>
2026-03-25 23:23:14 +0700

    fix/duplicate-code

--- src/components/common/BrandsCategory.jsx
+++ src/components/common/BrandsCategory.jsx
@@ -278,7 +278,7 @@ export default function BrandsCategory() {
           ))}
         </div>
       )} */}
-
+      {/*  */}
       {loading ? (
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-6 lg:gap-20">
           {Array.from({ length: 8 }).map((_, i) => (
